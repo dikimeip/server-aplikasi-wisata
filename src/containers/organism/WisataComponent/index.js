3ebb7b6890f950bbd42286573d8c69a29c42f778
dikimeip <pm159145@gmail.com>
2020-01-07 10:03:37 +0700

    membuat fungsi untuk get data dari api

---
+++ src/containers/organism/WisataComponent/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const WisataComponent = () => {
+    return(
+        <table className="table table-hover">
+            <tr>
+                <th>NAMA WISATA</th>
+                <th>TANGGAL</th>
+                <th>MAPS</th>
+                <th>FOTO</th>
+                <th>AKSI</th>
+            </tr>
+        </table>
+    )
+}
+
+export default  WisataComponent
--- src/containers/pages/Wisata/index.js
+++ src/containers/pages/Wisata/index.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import Navbar from '../../organism/Navbar'
+import Axios from 'axios'
 
 class Wisata extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            wisata : []
+        }
+    }
+
+    componentDidMount = () => {
+        Axios.get('http://localhost/api_pariwisata/wisata.php').then(res => {
+            console.log(res.data.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
         return (
             <div>
