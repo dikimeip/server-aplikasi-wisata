3e550552e4e2a931d3e5b1df34974236454a9933
dikimeip <pm159145@gmail.com>
2020-01-07 11:00:01 +0700

    membuuat fungsi untuk tambah data wisata

--- src/containers/pages/TambahWisata/index.js
+++ src/containers/pages/TambahWisata/index.js
@@ -1,7 +1,55 @@
 import React, { Component } from 'react'
 import Navbar from '../../organism/Navbar'
+import Axios from 'axios'
 
 class TambahWisata extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            nama : "",
+            tarik : "",
+            fasilitas :"",
+            akses : "",
+            map : "",
+            foto : "",
+            file : {
+                fto:""
+            }
+        }
+    }
+
+    inputChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    imageChange = (e) => {
+        this.setState({
+            foto: e.target.files[0].name,
+            file: {
+                fto: e.target.files[0]
+            }
+        })
+    }
+
+    handlerSummit = (e) => {
+        e.preventDefault()
+        const formdata = new FormData
+        formdata.append('foto',this.state.file.fto,this.state.foto)
+        Axios.post('http://localhost/api_pariwisata/file_pariwisata.php',formdata)
+        Axios.post('http://localhost/api_pariwisata/tambah_wisata.php',this.state).then(res => {
+            console.log(res)
+            if (res.data.id === 1) {
+                this.props.history.push('/wisata')
+            } else {
+                console.log('error')
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     render() {
         return (
             <div>
@@ -13,30 +61,30 @@ class TambahWisata extends Component {
                             <div className="panel-body">
                                 <h1 className="text-center">TAMBAH DATA</h1>
                                 <hr />
-                                <form>
+                                <form onSubmit={this.handlerSummit}>
                                     <div className="form-group">
                                         <label>Masukan Nama Wisata</label>
-                                        <input className="form-control" type="text" name="nama"  />
+                                        <input className="form-control" type="text" name="nama" onChange={this.inputChange}  />
                                     </div>
                                     <div className="form-group">
                                         <label>Masukan Daya Tarik</label>
-                                        <textarea className="form-control"></textarea>
+                                        <textarea className="form-control" name="tarik" onChange={this.inputChange}></textarea>
                                     </div>
                                     <div className="form-group">
                                         <label>Masukan Fasilitas Pariwisata</label>
-                                        <textarea className="form-control"></textarea>
+                                        <textarea className="form-control" name="fasilitas" onChange={this.inputChange}></textarea>
                                     </div>
                                     <div className="form-group">
                                         <label>Masukan Akses Wisata</label>
-                                        <textarea className="form-control"></textarea>
+                                        <textarea className="form-control" name="akses" onChange={this.inputChange}></textarea>
                                     </div>
                                     <div className="form-group">
                                         <label>Masukan Maps Long Lang</label>
-                                        <input className="form-control" type="text" name="maps"  />
+                                        <input className="form-control" type="text" name="map"  onChange={this.inputChange} />
                                     </div>
                                     <div className="form-group">
                                         <label>Masukan foto</label>
-                                        <input name="foto" className="form-control" type="file"></input>
+                                        <input name="foto" className="form-control" type="file" onChange={this.imageChange}></input>
                                     </div>
                                     <input type="submit" value="LOGIN" className="btn btn-info" />
                                 </form>
