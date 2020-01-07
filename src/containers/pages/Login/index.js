1eff2c54cc2531d42718cbf623eb2e96381e7e3e
dikimeip <pm159145@gmail.com>
2020-01-07 09:21:54 +0700

    melakukan configurasi untuk melakukan login

--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
 
-class Login extends Component{
-    render(){
-        return(
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            uname: "",
+            pass: ""
+        }
+    }
+
+    inputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handlerSubmit = (event) => {
+        event.preventDefault()
+        Axios.post('http://localhost/api_pariwisata/login.php',this.state).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+
+
+    render() {
+        return (
             <div>
                 <div className="row">
                     <div className="col-md-4"></div>
@@ -10,16 +36,18 @@ class Login extends Component{
                         <div className="panel panel-info">
                             <div className="panel-body">
                                 <h1 className="text-center">Login Page</h1>
-                                <hr/>
-                                <div className="form-group">
-                                    <label>Masukan Username</label>
-                                    <input className="form-control" type="text" name="uname" />
-                                </div>
-                                <div className="form-group">
-                                    <label>Masukan Username</label>
-                                    <input className="form-control" type="text" name="pswd" />
-                                </div>
-                                <button className="btn btn-info">LOGIN</button>
+                                <hr />
+                                <form onSubmit={this.handlerSubmit}>
+                                    <div className="form-group">
+                                        <label>Masukan Username</label>
+                                        <input className="form-control" type="text" name="uname" onChange={this.inputChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Masukan Username</label>
+                                        <input className="form-control" type="text" name="pass" onChange={this.inputChange} />
+                                    </div>
+                                    <input type="submit" value="LOGIN" className="btn btn-info" onSubmit={this.handlerSubmit} />
+                                </form>
                             </div>
                         </div>
                     </div>
